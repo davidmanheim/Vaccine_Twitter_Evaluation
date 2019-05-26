d4ae80677b8529083906690bffa53e25098c5105
David Manheim <davidmanheim@gmail.com>
2019-05-26 12:35:33 +0300

    Updated version of preregistration document, with additional notes. (Corrected to include changed draft!)

--- Retrieve Data.py
+++ Retrieve Data.py
@@ -1,39 +1,93 @@
 __author__ = 'dmanheim'
 
 import twitter
-
-import oauth2 as oauth
 import time
+# import oauth2 as oauth
+# import time
 import json
 from PrivateKeys import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
 
-def get_tweets(api=None, screen_name=None):
-    timeline = api.GetUserTimeline(screen_name=screen_name, count=200, include_rts=1)
-    earliest_tweet = min(timeline, key=lambda x: x.id).id
-    print("getting tweets before:", earliest_tweet, " for user:", screen_name)
+# This script retrieves the initial dataset of tweets from members of the WHO VSN list.
 
-    while True:
-        tweets = api.GetUserTimeline(
-            screen_name=screen_name, max_id=earliest_tweet, count=200, include_rts=1
-        )
-        new_earliest = min(tweets, key=lambda x: x.id).id
+def get_tweets(api=None, screen_name=None, Userid=None):
+    if Userid==None:
+        timeline = api.GetUserTimeline(screen_name=screen_name, count=200, include_rts=1)
+        try:
+            earliest_tweet = min(timeline, key=lambda x: x.id).id
+        except ValueError:
+            print('No earliest Tweet for user')
+            return 0
+        print("getting tweets before:", earliest_tweet, " for user:", screen_name)
 
-        if not tweets or new_earliest == earliest_tweet:
-            break
-        else:
-            earliest_tweet = new_earliest
-            print("getting tweets before:", earliest_tweet)
-            timeline += tweets
+        while True:
+            tweets = api.GetUserTimeline(
+                screen_name=screen_name, max_id=earliest_tweet, count=200, include_rts=1
+            )
+            new_earliest = min(tweets, key=lambda x: x.id).id
 
+            if not tweets or new_earliest == earliest_tweet:
+                break
+            else:
+                earliest_tweet = new_earliest
+                print("getting tweets before:", earliest_tweet)
+                timeline += tweets
+    else:
+        try:
+            timeline = api.GetUserTimeline(user_id=Userid, count=200, include_rts=1)
+        except twitter.error.TwitterError as TE:
+            print("Error for user: ", Userid)
+            if TE.message == "Not authorized.":
+                print("Not authorized, next.")
+                return 0
+            elif TE[0]['code'] == 88:
+                print('sleeping of 15 minutes')
+                time.sleep(60*15)
+                pass
+            return 0
+        try:
+            earliest_tweet = min(timeline, key=lambda x: x.id).id
+        except ValueError:
+            print('No earliest Tweet for user')
+            return 0
+
+        print("getting tweets before: ", earliest_tweet, " for user: ", Userid)
+
+        while True:
+            try:
+                tweets = api.GetUserTimeline(
+                    user_id=Userid, max_id=earliest_tweet, count=200, include_rts=1
+                )
+            except twitter.error.TwitterError as TE:
+                print("Error for user: ", Userid)
+                if TE.message == "Not authorized.":
+                    print("Not authorized, next.")
+                    return 0
+                elif "Rate limit" in TE:
+                    print('sleeping of 15 minutes')
+                    time.sleep(60 * 15)
+                    pass
+                return 0
+            try:
+                new_earliest = min(tweets, key=lambda x: x.id).id
+            except ValueError:
+                print('No more earliest Tweet for user')
+                break
+            if not tweets or new_earliest == earliest_tweet:
+                break
+            else:
+                earliest_tweet = new_earliest
+                print("getting tweets before:", earliest_tweet)
+                timeline += tweets
     return timeline
 
 
 def get_followers(api=None, screen_name=None):
-    users = api.GetFollowers(screen_name=screen_name) #Should be using User IDs, ideally.
+    users = api.GetFollowers(screen_name=screen_name)  # Should be using User IDs, ideally.
     return users
 
-if __name__ == "__main__":
+
+def run_initial():
     api = twitter.Api(
         CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, sleep_on_rate_limit=True
     )
@@ -41,33 +95,123 @@ if __name__ == "__main__":
     print(screen_name)
     VSN_Memberlist = get_followers(api, screen_name)
 
-    #We list what we have already, to exclude them.
-    Retrieved = {'dfeatamr', 'Diamantius', 'JiayaoL', 'filiamd', 'MamasMaestras', 'VirologyComics', 'CaringforKids', 'DrSusanNasif', 'LightForRiley', 'Utzy101', 'MenBAction', 'duronronron', 'mirkoclaus', 'aspsnb0816', 'SBohtToutant', 'SarahIOW', 'MaddieScomms', 'GrupoNumoca', 'snehiil', 'EpiRen', 'iboostimmunity', 'sabinvaccine', 'okpedaniels', 'GuciGC', 'Muhamma56627761', 'aneinternationa', 'Navaneetlovein1', 'tina_maryland', 'INTREPYLLC', 'loic_pfister', 'rajatgarg123', 'travelhealthdoc', 'PergamIC', 'clinicaimunita', 'justindknott', 'prof_brunt', 'ThomsonAngus', 'ChandniSondagar', 'NHSImmuniseScot', 'NotClickbat', 'danyalmartin', 'D_Lawrencium_G', 'keepgoi16968144', 'IIBCE', 'maimaikelly', 'mariojpanelli', 'BPalache', 'Epiconcept', 'HSEschoolsteam', 'chersvacca', 'AMurrayAuthor', 'KongKhL', 'NAV_igator', 'nurSYS', 'mratna_chai', 'PaolaCella3', 'drvikasmadaan', 'VaccineUK', 'Immunize_USA', 'HealthHarmonie', 'sineadorourke', 'jennifermiram18', 'MeaslesRubella', 'carlosdsuze', 'immunifyme', 'Finnpal', 'cat_stacey', 'katejb', 'BTCOConnor', 'CVN_UK', 'CAinexile', 'VEC_ubc', 'roberto_ieraci', 'ConRubellaSyndr', 'mdkimhsdubai', 'parismalou', 'SorayaCastroLL', 'TellurianBoy', 'FaziaTadount', 'zammit_marc', 'CANImmunize', 'DrNancyM_CDC', 'yurukov', 'huangwt', 'HealthWatch123', 'adadecarom', 'LaSF2H', 'merusheel', 'Eilidh257', 'TheBag70', 'Vaccinologist', 'MamaEn_Apuros', 'kath2cats', 'TeddyFreddy11', 'gyncsm', 'PepePediatre', 'StorageVaccines', 'Imms_Emma', 'JustynaMajek', 'MamasPara', 'SCaplinskas', 'jhaywoodcollett', 'LupeCastillo8', 'HelenEBedford', 'jcjimenop', 'jvalaball', 'juana_2012', 'DayaniGalato', 'Anya_Nori', 'DefeatDD', 'saranyavac', 'MrBGRK', 'Dr_roguetwo', 'designoversense', 'lehnent', 'kobaprio', 'DanaMur2', 'olga_rostkowska', 'TFX201X', 'olgmns', 'mona20_mona', 'ShotAtLife', 'PCIssuesAnswers', 'Dont_Go_Viral', 'Virginiatey', 'Lab_Diagnostics', 'NICU_doc_salone', 'RobbButler2', 'VaccinesEurope', 'tptrnn', 'ACT4Meningitis', 'basiaopalska', 'precioussoul07', 'Alireza26662033', 'rubialexat', 'SE_Mamelund', 'Pediatra2punto0', 'eddiedacosta2', 'DraAChvez', 'Doctors_NS', 'ToniPdevacunas', 'elisantosdorado', 'mmgassociati', 'MeissaVaccines', 'h3mclean', 'ZulfiqarEpi', 'vaccines4life', 'NilzaRojasA', 'DrToddWo', 'VacciniRIV', 'InfluenzaHub', 'azdi_16', 'Rosybel69', 'alahari_lokesh', 'RESCEUproject', 'EileenRushe', 'ImmunizeCOKids', 'luppi_n', 'ClarMaure', 'Adiela_SV', 'RachMacIntosh85', 'MeningitisCA', 'Gringrid', 'YTotskaya', 'PriscillaLynch', 'ChrisNorley', 'healthykids11', 'drlucydeng', 'maldonaitsabes', 'SophieCremen', 'Doctorshaib', 'doinaazoicai', 'Vaxyourfam', 'marisolfarma', 'RachelAlter007', 'AlokBhatia2008', 'cima_study', 'stephcru', 'UNjobs', 'learntherisks', 'AidGanesh', 'Petekash2', 'PEI_Germany', 'ahmrc', 'PrecVaccProgram', 'finnegag', 'WichmannOle', 'Kamlashankerv', 'KaitlinSundling', 'ConPediatriaSLP', 'Johnmcg26591878', 'IDEpiPhD', 'EpicHealthNews', 'pinabertoglia', 'PaulVanBuynder', 'NedretEmiroglu', 'BactiVac', 'pugliesemd', 'karieyoungdahl', 'adelebaleta', 'COMOmeningitis', 'Dorota_M_Wojcik', 'SAFIAIB38469578', 'rolandoug', 'StilesJoseph', 'BallatoEdson', 'HPVBoard', 'Saapedispenser', 'soyrami', 'UnityConsortium', 'Pame_Q', 'MaireadOConnor8', 'naveenthacker', 'TaubertLab', 'MichaelBuratovi', 'The_ISHA', 'MartinaAnzaghe', 'Bhanwar41283624', 'Vaccination_UK', 'wouterhavinga', 'CaScreen_carrie', 'IPitcher', 'MVT_InnoFund', 'carlyweeks', 'JoTrust', 'TakeThatChem', 'SuffolkImmsTeam', 'edsonsinuhe', 'samicarlson', 'H_HongPhD', 'FrkGiovanetti', 'ThisIsCANVax', 'AngelesRzvz', 'simonsdlj', 'kimmykims33', 'JacoboMendioroz', 'SciencePharmer', 'FormularyIE', 'nancycampen', 'midha_abhishek', 'ARahman_300', 'DrJennersHouse', 'CWPTSolihullImm', 'LaycockSandra', 'InfovacFrance', 'DrSaiSatish', 'jobiebudd', 'marietcasey', 'ZuhurBalayah', 'APPGVaccination', 'SoniaBoender', 'hschlent', 'ImmunizeTXW', 'mfprada', 'Larry_Svenson', 'Atlantis1331', 'cacupc', 'VaccinateIN', 'jackihartley', 'CFragilite', 'pharmamafia', 'drive_eu', 'tapasnair', 'rishmy2', 'OjamaaMikk', 'MongeHernando', 'Mariagreene8', 'historyvaccines', 'Minozennn', 'NIZP_PZH', 'JejeLouvetou', 'HenningTveit', 'ahbsam6', 'vaccines411', 'STOPmeningitis', 'LafayettePHNerd'}
+    # We list what we have already, to exclude them.
+    Retrieved = {'dfeatamr', 'Diamantius', 'JiayaoL', 'filiamd', 'MamasMaestras', 'VirologyComics', 'CaringforKids',
+                 'DrSusanNasif', 'LightForRiley', 'Utzy101', 'MenBAction', 'duronronron', 'mirkoclaus', 'aspsnb0816',
+                 'SBohtToutant', 'SarahIOW', 'MaddieScomms', 'GrupoNumoca', 'snehiil', 'EpiRen', 'iboostimmunity',
+                 'sabinvaccine', 'okpedaniels', 'GuciGC', 'Muhamma56627761', 'aneinternationa', 'Navaneetlovein1',
+                 'tina_maryland', 'INTREPYLLC', 'loic_pfister', 'rajatgarg123', 'travelhealthdoc', 'PergamIC',
+                 'clinicaimunita', 'justindknott', 'prof_brunt', 'ThomsonAngus', 'ChandniSondagar', 'NHSImmuniseScot',
+                 'NotClickbat', 'danyalmartin', 'D_Lawrencium_G', 'keepgoi16968144', 'IIBCE', 'maimaikelly',
+                 'mariojpanelli', 'BPalache', 'Epiconcept', 'HSEschoolsteam', 'chersvacca', 'AMurrayAuthor', 'KongKhL',
+                 'NAV_igator', 'nurSYS', 'mratna_chai', 'PaolaCella3', 'drvikasmadaan', 'VaccineUK', 'Immunize_USA',
+                 'HealthHarmonie', 'sineadorourke', 'jennifermiram18', 'MeaslesRubella', 'carlosdsuze', 'immunifyme',
+                 'Finnpal', 'cat_stacey', 'katejb', 'BTCOConnor', 'CVN_UK', 'CAinexile', 'VEC_ubc', 'roberto_ieraci',
+                 'ConRubellaSyndr', 'mdkimhsdubai', 'parismalou', 'SorayaCastroLL', 'TellurianBoy', 'FaziaTadount',
+                 'zammit_marc', 'CANImmunize', 'DrNancyM_CDC', 'yurukov', 'huangwt', 'HealthWatch123', 'adadecarom',
+                 'LaSF2H', 'merusheel', 'Eilidh257', 'TheBag70', 'Vaccinologist', 'MamaEn_Apuros', 'kath2cats',
+                 'TeddyFreddy11', 'gyncsm', 'PepePediatre', 'StorageVaccines', 'Imms_Emma', 'JustynaMajek', 'MamasPara',
+                 'SCaplinskas', 'jhaywoodcollett', 'LupeCastillo8', 'HelenEBedford', 'jcjimenop', 'jvalaball',
+                 'juana_2012', 'DayaniGalato', 'Anya_Nori', 'DefeatDD', 'saranyavac', 'MrBGRK', 'Dr_roguetwo',
+                 'designoversense', 'lehnent', 'kobaprio', 'DanaMur2', 'olga_rostkowska', 'TFX201X', 'olgmns',
+                 'mona20_mona', 'ShotAtLife', 'PCIssuesAnswers', 'Dont_Go_Viral', 'Virginiatey', 'Lab_Diagnostics',
+                 'NICU_doc_salone', 'RobbButler2', 'VaccinesEurope', 'tptrnn', 'ACT4Meningitis', 'basiaopalska',
+                 'precioussoul07', 'Alireza26662033', 'rubialexat', 'SE_Mamelund', 'Pediatra2punto0', 'eddiedacosta2',
+                 'DraAChvez', 'Doctors_NS', 'ToniPdevacunas', 'elisantosdorado', 'mmgassociati', 'MeissaVaccines',
+                 'h3mclean', 'ZulfiqarEpi', 'vaccines4life', 'NilzaRojasA', 'DrToddWo', 'VacciniRIV', 'InfluenzaHub',
+                 'azdi_16', 'Rosybel69', 'alahari_lokesh', 'RESCEUproject', 'EileenRushe', 'ImmunizeCOKids', 'luppi_n',
+                 'ClarMaure', 'Adiela_SV', 'RachMacIntosh85', 'MeningitisCA', 'Gringrid', 'YTotskaya', 'PriscillaLynch',
+                 'ChrisNorley', 'healthykids11', 'drlucydeng', 'maldonaitsabes', 'SophieCremen', 'Doctorshaib',
+                 'doinaazoicai', 'Vaxyourfam', 'marisolfarma', 'RachelAlter007', 'AlokBhatia2008', 'cima_study',
+                 'stephcru', 'UNjobs', 'learntherisks', 'AidGanesh', 'Petekash2', 'PEI_Germany', 'ahmrc',
+                 'PrecVaccProgram', 'finnegag', 'WichmannOle', 'Kamlashankerv', 'KaitlinSundling', 'ConPediatriaSLP',
+                 'Johnmcg26591878', 'IDEpiPhD', 'EpicHealthNews', 'pinabertoglia', 'PaulVanBuynder', 'NedretEmiroglu',
+                 'BactiVac', 'pugliesemd', 'karieyoungdahl', 'adelebaleta', 'COMOmeningitis', 'Dorota_M_Wojcik',
+                 'SAFIAIB38469578', 'rolandoug', 'StilesJoseph', 'BallatoEdson', 'HPVBoard', 'Saapedispenser',
+                 'soyrami', 'UnityConsortium', 'Pame_Q', 'MaireadOConnor8', 'naveenthacker', 'TaubertLab',
+                 'MichaelBuratovi', 'The_ISHA', 'MartinaAnzaghe', 'Bhanwar41283624', 'Vaccination_UK', 'wouterhavinga',
+                 'CaScreen_carrie', 'IPitcher', 'MVT_InnoFund', 'carlyweeks', 'JoTrust', 'TakeThatChem',
+                 'SuffolkImmsTeam', 'edsonsinuhe', 'samicarlson', 'H_HongPhD', 'FrkGiovanetti', 'ThisIsCANVax',
+                 'AngelesRzvz', 'simonsdlj', 'kimmykims33', 'JacoboMendioroz', 'SciencePharmer', 'FormularyIE',
+                 'nancycampen', 'midha_abhishek', 'ARahman_300', 'DrJennersHouse', 'CWPTSolihullImm', 'LaycockSandra',
+                 'InfovacFrance', 'DrSaiSatish', 'jobiebudd', 'marietcasey', 'ZuhurBalayah', 'APPGVaccination',
+                 'SoniaBoender', 'hschlent', 'ImmunizeTXW', 'mfprada', 'Larry_Svenson', 'Atlantis1331', 'cacupc',
+                 'VaccinateIN', 'jackihartley', 'CFragilite', 'pharmamafia', 'drive_eu', 'tapasnair', 'rishmy2',
+                 'OjamaaMikk', 'MongeHernando', 'Mariagreene8', 'historyvaccines', 'Minozennn', 'NIZP_PZH',
+                 'JejeLouvetou', 'HenningTveit', 'ahbsam6', 'vaccines411', 'STOPmeningitis', 'LafayettePHNerd'}
 
     Remaining = [u for u in VSN_Memberlist if u.screen_name not in Retrieved]
 
     # print([u.screen_name for u in Memberlist_Subset])
 
     with open('tweet_data.json', 'w+') as f:
-        for u in Remaining: #VSN_Memberlist:
-            #Need to check if protected!
-            #Also, crashes when user has never tweeted, i.e. Feli56686027 - I now check for that.
-            #Also, it crashes anyways and I'm doing this in multiple steps / files.
+        for u in Remaining:  # VSN_Memberlist:
+            # Need to check if protected!
+            # Also, crashes when user has never tweeted, i.e. Feli56686027 - I now check for that.
+            # Also, it crashes anyways and I'm doing this in multiple steps / files.
 
             if not u.protected:
                 if u.statuses_count > 0:
                     timeline = get_tweets(api=api, screen_name=u.screen_name)
 
-                 # Should I create a per-user file?
+                    # Should I create a per-user file?
                     for tweet in timeline:
-                         f.write(json.dumps(tweet._json))
-                         f.write('\n')
+                        f.write(json.dumps(tweet._json))
+                        f.write('\n')
 
-                    #Get Rate data and sleep if we're too close.
-                    #time.sleep(15) #Wait a quarter minute per user so that the rate limit isn't hit.
+                    # Get Rate data and sleep if we're too close.
+                    # time.sleep(15) #Wait a quarter minute per user so that the rate limit isn't hit.
                     # This is a ridiculous hack. Fixed by changing the API interface to sleep on hitting rate limit
 
         # I will need to find replies to and quotes of any tweet in that set in the given time frame. (For that I need IDs.)
 
 
 # This dataset includes tweets before & after our intended timeline. It needs to be filtered.
+
+
+# These are now in a file, and it uses IDs, not screen names.
+#     Replied_To = []
+
+if __name__ == "__main__":
+# run_initial() # Did this already
+
+    api = twitter.Api(
+        CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, sleep_on_rate_limit=True
+    )
+
+
+
+    # I need to fetch data for all users in Replied_To
+    # It contains duplicates, and may have users whose data was already retrieved.
+    Excluded_file = open("Updating_Pulled_Exclude_IDs.json", 'r')
+    for line in Excluded_file:
+        excludeIDs = json.loads(line)
+    Excluded_file.close()
+    ReplyID_file = open("Initial_Replied_IDList.json", "r")
+    for line in ReplyID_file:
+        Replied_To_ID = json.loads(line)
+    ReplyID_file.close()
+
+    for userID in Replied_To_ID:
+        # Need to check if protected!
+        # Also, crashes when user has never tweeted, i.e. Feli56686027 - I now check for that.
+        # Also, it crashes anyways and I'm doing this in multiple steps / files.
+
+        if userID not in excludeIDs:
+            timeline = get_tweets(api=api, Userid=userID)
+            if timeline==0:
+                print("Skipping this, excluding user.")
+            else:
+                OutFile = "Data\Tweets" + str(userID) + ".json"
+                with open(OutFile, 'a') as f:
+                    for tweet in timeline:
+                        f.write(json.dumps(tweet._json))
+                        f.write('\n')
+                            # Get Rate data and sleep if we're too close.
+                            # time.sleep(15) #Wait a quarter minute per user so that the rate limit isn't hit.
+                            # This is a ridiculous hack. Fixed by changing the API interface to sleep on hitting rate limit
+                excludeIDs.append(userID)
+            with open("Updating_Pulled_Exclude_IDs.json", 'w+') as f:
+                f.write(json.dumps(excludeIDs)) #Replace old list with new one. If there is a crash, this helps!
+
+# I will need to find replies to and quotes of any tweet in that set in the given time frame. (For that I need IDs.)
--- Tweet File Read.py
+++ Tweet File Read.py
@@ -2,15 +2,16 @@ import json
 
 # I got a lot of data, but then it cut out. I need to get the rest.
 #FILE = 'WHO_VSN_tweet_data.json' #for testing, I'm using this, not the full set.
-FILES[1] = 'tweet_data_Partial1.json'
-FILES[2] = 'tweet_data_Partial2.json'
-FILES[3] = 'tweet_data_Partial3.json'
-FILES[4] = 'tweet_data_Partial4.json'
-FILES[5] = 'tweet_data_Partial5.json'
+FILES = list()
+FILES.append('tweet_data_Partial1.json')
+FILES.append('tweet_data_Partial2.json')
+FILES.append('tweet_data_Partial3.json')
+FILES.append('tweet_data_Partial4.json')
+FILES.append('tweet_data_Partial5.json')
 
-def pull_users(file_list, ):
+def pull_users(file_list):
     userlist=set()
-
+    print(file_list)
     for file in file_list:
         with open(file) as json_file:
             for line in json_file:
@@ -19,6 +20,61 @@ def pull_users(file_list, ):
                     userlist.add(tweet["user"]["screen_name"])
     return userlist
 
-print(pull_users(FILES))
+Retrieved = pull_users(FILES)
+print(Retrieved)
 
+# List retrieved from Tweet File Read
+# Retrieved = ['DrToddWo', 'BalibreaNuria', 'h3mclean', 'CaScreen_carrie', 'SuffolkImmsTeam', 'luppi_n', 'TheBag70', 'LaycockSandra',
+#  'InfovacFrance', 'aneinternationa', 'merusheel', 'D_Lawrencium_G', 'Epiconcept', 'Immunize_USA', 'FaziaTadount',
+#  'HSEschoolsteam', 'adelebaleta', 'COMOmeningitis', 'NHSImmuniseScot', 'DayaniGalato', 'Vaccinologist',
+#  'PrecVaccProgram', 'tina_maryland', 'BallatoEdson', 'DattaChaudhuri', 'rajatgarg123', 'juana_2012', 'tptrnn',
+#  'mariojpanelli', 'kath2cats', 'CWPTSolihullImm', 'IPitcher', 'MongeHernando', 'LightForRiley', 'ACT4Meningitis',
+#  'ISahinovic', 'JejeLouvetou', 'ConRubellaSyndr', 'IIBCE', 'SBohtToutant', 'ThomsonAngus', 'RachMacIntosh85',
+#  'maldonaitsabes', 'jackihartley', 'pinabertoglia', 'HealthWatch123', 'mirkoclaus', 'midha_abhishek', 'INTREPYLLC',
+#  'BTCOConnor', 'Utzy101', 'Muhamma56627761', 'AlokBhatia2008', 'xcel_regal', 'pugliesemd', 'KaitlinSundling',
+#  'cima_study', 'CVN_UK', 'cacupc', 'katejb', 'muldersm', 'ZulfiqarEpi', 'SorayaCastroLL', 'Pame_Q', 'ZuhurBalayah',
+#  'TFX201X', 'adeyeye_eo', 'NedretEmiroglu', 'roberto_ieraci', 'immunifyme', 'jobiebudd', 'NotClickbat', 'naveenthacker',
+#  'jvalaball', 'PriscillaLynch', 'elisantosdorado', 'LafayettePHNerd', 'HealthHarmonie', 'PCIssuesAnswers', 'vipintukur',
+#  'AMurrayAuthor', 'VaccinesToday', 'PaulVanBuynder', 'Mariagreene8', 'Finnpal', 'Johnmcg26591878', 'SAFIAIB38469578',
+#  'AidGanesh', 'ChrisNorley', 'okpedaniels', 'olga_rostkowska', 'jcjimenop', 'kimmykims33', 'nancycampen',
+#  'UnityConsortium', 'MartinaAnzaghe', 'SciencePharmer', 'mratna_chai', 'Danielot', 'JiayaoL', 'NICU_doc_salone',
+#  'CaringforKids', 'SarahIOW', 'rx_mph', 'ToniPdevacunas', 'travelhealthdoc', 'carlyweeks', 'mdkimhsdubai', 'Gringrid',
+#  'Alireza26662033', 'clinicaimunita', 'zuberp1', 'ARahman_300', 'keepgoi16968144', 'maimaikelly', 'Atlantis1331',
+#  'Adiela_SV', 'carlosdsuze', 'CANImmunize', 'tapasnair', 'JacoboMendioroz', 'karieyoungdahl', 'sineadorourke',
+#  'RESCEUproject', 'basiaopalska', 'edsonsinuhe', 'ChandniSondagar', 'HPVBoard', 'MeissaVaccines', 'wouterhavinga',
+#  'IDEpiPhD', 'EpicHealthNews', 'PergamIC', 'EileenRushe', 'VaccinesEurope', 'simonsdlj', 'PEI_Germany',
+#  'Bhanwar41283624', 'saranyavac', 'marisolfarma', 'adadecarom', 'MVT_InnoFund', 'LaSF2H', 'eddiedacosta2', 'VacciniRIV',
+#  'drvikasmadaan', 'DefeatDD', 'Eilidh257', 'Minozennn', 'Doctors_NS', 'SE_Mamelund', 'RobbButler2', 'stephcru',
+#  'Pediatra2punto0', 'ShotAtLife', 'H_HongPhD', 'vaccines4life', 'danyalmartin', 'samicarlson', 'designoversense',
+#  'TakeThatChem', 'DrSusanNasif', 'iboostimmunity', 'FormularyIE', 'mfprada', 'Dont_Go_Viral', 'Vaccination_UK', 'NCIRS',
+#  'botta_gabriela', 'Frasca74', 'jhaywoodcollett', 'TaubertLab', 'MamasPara', 'NIZP_PZH', 'MamasMaestras', 'UnmeshDr',
+#  'ICBDSR', 'HSEImm', 'TellurianBoy', 'GuciGC', 'huangwt', 'VaccineUK', 'KongKhL', 'yurukov', 'dfeatamr',
+#  'WHO_Europe_VPI', 'MrBGRK', 'ahbsam6', 'PaolaCella3', 'rubialexat', 'historyvaccines', 'SCaplinskas', 'parismalou',
+#  'ThisIsCANVax', 'GrupoNumoca', 'cat_stacey', 'lehnent', 'WichmannOle', 'Lab_Diagnostics', 'GermyGermyGermy',
+#  'mmgassociati', 'Imms_Emma', 'MeaslesRubella', 'VaccinateIN', 'StorageVaccines', 'ahmrc', 'duronronron', 'rolandoug',
+#  'drlucydeng', 'MichaelBuratovi', 'MamaEn_Apuros', 'Rosybel69', 'JustynaMajek', 'BPalache', 'sabinvaccine', 'BactiVac',
+#  'drive_eu', 'VaxResources', 'NilzaRojasA', 'ImmunizeCOKids', 'gyncsm', 'learntherisks', 'DraAChvez', 'prof_brunt',
+#  'SophieCremen', 'VEC_ubc', 'The_ISHA', 'Dr_roguetwo', 'olgmns', 'finnegag', 'SoniaBoender', 'vaccines411',
+#  'RachelAlter007', 'STOPmeningitis', 'VirologyComics', 'mona20_mona', 'Diamantius', 'alahari_lokesh', 'MaddieScomms',
+#  'jennifermiram18', 'MenBAction', 'InfluenzaHub', 'Simozzzzz', 'azdi_16', 'MaireadOConnor8', 'Kamlashankerv', 'UNjobs',
+#  'loic_pfister', 'Vaxyourfam', 'DrSaiSatish', 'ImmunizedotCa', 'Larry_Svenson', 'FrkGiovanetti', 'ClarMaure',
+#  'Navaneetlovein1', 'soyrami', 'rishmy2', 'StilesJoseph', 'Saapedispenser', 'APPGVaccination', 'LupeCastillo8',
+#  'PepePediatre', 'doinaazoicai', 'nurSYS', 'snehiil', 'HelenEBedford', 'TeddyFreddy11', 'CAinexile', 'DrJennersHouse',
+#  'Petekash2', 'justindknott', 'OjamaaMikk', 'WalleyRay', 'Dorota_M_Wojcik', 'gloriapagin', 'precioussoul07',
+#  'ElIngenieroCha1', 'Virginiatey', 'YTotskaya', 'AngelesRzvz', 'zammit_marc', 'aspsnb0816', 'Doctorshaib', 'chersvacca',
+#  'ImmunizeTXW', 'marietcasey', 'DanaMur2', 'healthykids11', 'hschlent', 'DrNancyM_CDC', 'kobaprio', 'MeningitisCA',
+#  'Anya_Nori', 'pharmamafia', 'CFragilite', 'EpiRen', 'NAV_igator', 'dey_aditi', 'NaTHNaC', 'ConPediatriaSLP', 'JoTrust',
+#  'HenningTveit', 'filiamd']
 
+# with open("Pulled_Exclude.json", 'w+') as f:
+#     f.write(json.dumps(Retrieved))
+#
+# fileX = open("WHO_VSN_Members.json", 'r')
+# for line in fileX:
+#     test = json.loads(line)
+# WHO_VSN_IDlist=list()
+# for u in test:
+#     WHO_VSN_IDlist.append(u["ID"])
+#
+# with open("Pulled_Exclude_IDs.json", 'w+') as f:
+#     f.write(json.dumps(WHO_VSN_IDlist))
